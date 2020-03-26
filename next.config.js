535a41ba29dfa97b9269d5f175edc469633f016a
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-03-25 18:03:20 -0700

    Fix random things

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const path = require("path")
+const withCSS = require("@zeit/next-css")
 
-module.exports = {
+module.exports = withCSS({
   pageExtensions: ["mdx", "jsx", "js", "ts", "tsx"],
   webpack(config) {
     // allow imports in js to be root-relative, like "components/.."
     config.resolve.modules.push(path.resolve("./src/"))
     return config
   },
-}
+})
---
+++ src/pages/resume.tsx
@@ -0,0 +1,7 @@
+const Resume = () => (
+  <div>
+    <embed src="public/resume-rr.pdf" type="application/pdf" width="100%" height="800px" />
+  </div>
+)
+
+export default Resume
