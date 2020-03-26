1cfa2aefc866aaf7c196395c9ff14df8a63bfacf
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-03-25 18:44:34 -0700

    build errors

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 const path = require("path")
-const withCSS = require("@zeit/next-css")
 
-module.exports = withCSS({
+module.exports = {
   pageExtensions: ["mdx", "jsx", "js", "ts", "tsx"],
   webpack(config) {
     // allow imports in js to be root-relative, like "components/.."
     config.resolve.modules.push(path.resolve("./src/"))
     return config
   },
-})
+}
