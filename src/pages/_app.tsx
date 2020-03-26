d0ce97ed1b24e629d93e50df230c8550cc62160d
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-03-25 18:30:48 -0700

    Fix random things

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,9 @@
-import { config } from "@fortawesome/fontawesome-svg-core"
-import "@fortawesome/fontawesome-svg-core/styles.css" // Import the CSS
-
 import Footer from "components/Footer"
 import Header from "components/Header"
 import SocialNav from "components/SocialNav"
 
 import "./styles.css"
 
-config.autoAddCss = false
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
