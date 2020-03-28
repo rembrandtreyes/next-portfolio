ed9ba1909348e64fa12bf95267457a6112ce6dc4
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-03-27 17:50:23 -0700

    Add styles for Mobile social menu

--- src/components/SocialNav/SocialNav.tsx
+++ src/components/SocialNav/SocialNav.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub, faTwitter, faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
-import styled from "styled-components"
+import { faUser, faTimes } from "@fortawesome/free-solid-svg-icons"
+
+import { SocialNavWrapper, SocialLinks, SocialMenu } from "./SocialNavStyles"
 
 const socials = [
   { name: "GitHub", icon: faGithub, link: "https://github.com/rembrandtreyes" },
@@ -9,46 +12,28 @@ const socials = [
   { name: "Instagram", icon: faInstagram, link: "https://www.instagram.com/rembrandtreyes/" },
 ]
 
-const SocialNav = () => (
-  <SocialNavWrapper>
-    {socials.map(social => {
-      return (
-        <a key={social.name} href={social.link}>
-          <FontAwesomeIcon icon={social.icon} />
-        </a>
-      )
-    })}
-  </SocialNavWrapper>
-)
-
-export const SocialNavWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  position: fixed;
-  bottom: 32px;
-  left: 32px;
-
-  svg {
-    width: 24px !important;
-  }
-
-  a {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 38px;
-    width: 38px;
-    color: #222222;
-    font-size: 1.5em;
-    margin: 8px 8px;
-    border-radius: 50%;
-    transition: all cubic-bezier(0.4, 0, 0.2, 1) 500ms;
-
-    &:hover {
-      box-shadow: -2px -2px 5px rgba(255, 255, 255, 1), 2px 2px 5px rgba(0, 0, 0, 0.5),
-        -4px -4px 10px rgba(255, 255, 255, 1), 4px 4px 10px rgba(0, 0, 0, 0.25);
-    }
-  }
-`
+const SocialNav = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(prevIsOpen => !prevIsOpen)
+  return (
+    <>
+      <SocialNavWrapper open={isOpen}>
+        {socials.map(social => {
+          return (
+            <SocialLinks target="_blank" open={isOpen} key={social.name} href={social.link}>
+              <FontAwesomeIcon icon={social.icon} />
+            </SocialLinks>
+          )
+        })}
+      </SocialNavWrapper>
+      <SocialMenu onClick={toggle}>
+        <FontAwesomeIcon icon={isOpen ? faTimes : faUser} />
+        <span />
+        <span />
+        <span />
+      </SocialMenu>
+    </>
+  )
+}
 
 export default SocialNav
---
+++ src/components/SocialNav/SocialNavStyles.tsx
@@ -0,0 +1,86 @@
+import styled from "styled-components"
+
+export const SocialNavWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  bottom: 32px;
+  left: 32px;
+
+  @media screen and (max-width: 600px) {
+    bottom: 20px;
+    left: 4px;
+    transition: all cubic-bezier(0.4, 0, 0.2, 1) 500ms;
+    background-color: rgba(255, 255, 255, 0.97);
+    transform: ${({ open }) => open && "translate(-30%, 30%)"};
+    border-radius: ${({ open }) => open && "50%"};
+    width: ${({ open }) => (open ? "150px" : "50px")};
+    height: ${({ open }) => (open ? "150px" : "50px")};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  svg {
+    width: 24px !important;
+  }
+`
+
+export const SocialLinks = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 38px;
+  width: 38px;
+  color: #222222;
+  font-size: 1.5em;
+  margin: 8px 8px;
+  border-radius: 50%;
+  transition: all cubic-bezier(0.4, 0, 0.2, 1) 500ms;
+
+  &:hover {
+    box-shadow: -2px -2px 5px rgba(255, 255, 255, 1), 2px 2px 5px rgba(0, 0, 0, 0.5),
+      -4px -4px 10px rgba(255, 255, 255, 1), 4px 4px 10px rgba(0, 0, 0, 0.25);
+  }
+
+  @media screen and (max-width: 600px) {
+    position: absolute;
+    opacity: ${({ open }) => (open ? "1" : "0")};
+    margin: 0;
+    z-index: -10;
+
+    &:nth-child(1) {
+      transform: ${({ open }) => open && "translate(0px, -50px)"};
+    }
+    &:nth-child(2) {
+      transform: ${({ open }) => open && "translate(35px, -45px)"};
+    }
+    &:nth-child(3) {
+      transform: ${({ open }) => open && "translate(50px, -12px)"};
+    }
+    &:nth-child(4) {
+      transform: ${({ open }) => open && "translate(50px, 20px)"};
+    }
+  }
+`
+
+export const SocialMenu = styled.div`
+  position: fixed;
+  bottom: 20px;
+  left: 4px;
+  height: 50px;
+  width: 50px;
+  font-size: 1.5em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all cubic-bezier(0.4, 0, 0.2, 1) 500ms;
+
+  svg {
+    width: 24px !important;
+  }
+
+  @media screen and (min-width: 601px) {
+    display: none;
+  }
+`
