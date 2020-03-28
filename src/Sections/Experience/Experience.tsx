e6d7559fe09ebada71e6f99f77327060e260518b
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-03-28 12:25:02 -0700

    Add mobile styles for Experience section

--- src/Sections/Experience/Experience.tsx
+++ src/Sections/Experience/Experience.tsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react"
-import styled from "styled-components"
+
+import {
+  ExperienceWrapper,
+  WorkContent,
+  CompanyWrapper,
+  Company,
+  StyledButton,
+  StyledLine,
+  JobDuties,
+} from "./ExperienceStyles"
 
 const jobs = [
   {
@@ -15,7 +24,7 @@ const jobs = [
   },
   {
     position: "Lead Planogram Analyst",
-    dates: "January 2019 - Present",
+    dates: "October 2017 - January 2019",
     duties: [
       "Lead a team of 7 analysts who supported the category analysts for dry foods",
       "Created automation scripts that helped reduce repetitive tasks for analysts",
@@ -35,18 +44,26 @@ const Experience = () => {
     <ExperienceWrapper id="experience">
       <h2>
         <span role="img" aria-label="man-technologist">
-          👷
+          👨‍💻
         </span>
         Where I did my time
       </h2>
       <WorkContent>
-        <CompanyContent>
-          {TABS.map((company, index) => (
-            <StyledButton key={company} active={activeTab === index} type="button" onClick={() => setActiveTab(index)}>
-              {company}
-            </StyledButton>
-          ))}
-        </CompanyContent>
+        <CompanyWrapper>
+          <Company>
+            {TABS.map((company, index) => (
+              <StyledButton
+                key={company}
+                active={activeTab === index}
+                type="button"
+                onClick={() => setActiveTab(index)}
+              >
+                {company}
+              </StyledButton>
+            ))}
+          </Company>
+          <StyledLine activeTab={activeTab} />
+        </CompanyWrapper>
         <JobDuties>
           <h4>{jobs[activeTab].position}</h4>
           <p>{jobs[activeTab].dates}</p>
@@ -62,88 +79,5 @@ const Experience = () => {
     </ExperienceWrapper>
   )
 }
-
-export const ExperienceWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 80vh;
-  flex-direction: column;
-  transition: 250ms ease-in;
-
-  span {
-    margin-right: 16px;
-  }
-  h2 {
-    display: flex;
-    font-size: 2.25em;
-
-    &:after {
-      content: "";
-      display: block;
-      height: 1px;
-      width: 150px;
-      background-color: rgb(45, 57, 82);
-      position: relative;
-      top: 25px;
-      margin-left: 16px;
-    }
-  }
-`
-
-export const WorkContent = styled.div`
-  display: flex;
-  min-height: 500px;
-`
-
-export const JobDuties = styled.div`
-  width: 500px;
-  margin-left: 16px;
-  transition: 0.2s ease-in;
-  ul {
-    list-style: none;
-    padding-inline-start: 24px;
-  }
-
-  li {
-    display: flex;
-    line-height: 1.5;
-    margin-bottom: 8px;
-
-    p {
-      margin: 0;
-    }
-
-    &:before {
-      content: "▷";
-      padding-top: 2px;
-      margin-right: 8px;
-    }
-  }
-`
-
-export const CompanyContent = styled.span`
-  display: flex;
-  flex-direction: column;
-`
-
-export const StyledButton = styled.button<{ active: boolean }>`
-  background-color: transparent;
-  padding: 16px 0;
-  width: 150px;
-  border-style: hidden;
-  font-size: 16px;
-  border-left: ${({ active }) => (active ? "3px solid black" : "")};
-  font-weight: ${({ active }) => (active ? "bold" : "normal")};
-  opacity: ${({ active }) => (active ? "1" : "0.6")};
-  transition: 250ms ease-in;
-  &:focus {
-    outline: none;
-  }
-
-  &:active {
-    border-style: hidden;
-  }
-`
 
 export default Experience
