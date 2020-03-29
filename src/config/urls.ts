bfc67cbb83fc0fa02a99c5997fe94d3864a35d7d
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-03-28 19:01:30 -0700

    Add URLS to a const file

---
+++ src/config/urls.ts
@@ -0,0 +1,9 @@
+const paths = {
+  github: "https://www.github.com/rembrandtreyes",
+  linkedIn: "https://www.linkedin.com/in/rembrandtreyes",
+  twitter: "https://twitter.com/rembrandtreyes",
+  instagram: "https://www.instagram.com/rembrandtreyes",
+  portfolioRepo: "https://github.com/rembrandtreyes/next-portfolio",
+}
+
+export default paths
