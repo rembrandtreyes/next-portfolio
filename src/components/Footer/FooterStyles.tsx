9ec817149f0a577f775340d329554757c557e1e8
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-03-31 17:17:44 -0700

    Extract footer styles into its own file Add supporter links Add exact links to urls config file

---
+++ src/components/Footer/FooterStyles.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable import/prefer-default-export */
+import styled from "styled-components"
+
+export const FooterWrapper = styled.div`
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
