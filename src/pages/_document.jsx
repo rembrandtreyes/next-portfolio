b6c3bf54acd3c5232e1a8d95a6b5a37aa681961e
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-06-17 14:05:17 -0700

    remove unused react-ga from _document

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,8 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
 import { ServerStyleSheet } from "styled-components"
 
-import { initGA, logPageView } from "utils/analytics.js"
-
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
@@ -27,14 +25,6 @@ class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
-  }
-
-  componentDidMount() {
-    if (!window.GA_INITIALIZED) {
-      initGA()
-      window.GA_INITIALIZED = true
-    }
-    logPageView()
   }
 
   render() {
